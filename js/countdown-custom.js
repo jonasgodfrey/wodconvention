68fb3fc26b4d480587a2a1f8c3497d9e595a1959
Godfrey Jonas <41231839+jonasgodfrey@users.noreply.github.com>
2022-03-10 19:37:16 +0100

    first commit

---
+++ js/countdown-custom.js
@@ -0,0 +1,7 @@
+(function($) {	
+	'use strict';
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2022, 2, 6, 1, )}); // year, month, date, hour
+        });
+})(jQuery);
+
